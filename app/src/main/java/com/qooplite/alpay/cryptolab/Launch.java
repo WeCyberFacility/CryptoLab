be96bb3f37be4786d8787a95156297476cb91193
Alpay <cyberfacilitygroup@gmail.com>
2019-07-12 22:55:18 +0200

    Fast Fertig .. CREDITS FEHLEN NOCH

--- app/src/main/java/com/qooplite/alpay/cryptolab/Launch.java
+++ app/src/main/java/com/qooplite/alpay/cryptolab/Launch.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Handler;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -27,6 +28,8 @@ public class Launch extends AppCompatActivity {
         setContentView(R.layout.activity_launch);
         Key = (ImageView) findViewById(R.id.schluesselLogo);
         text = (ImageView) findViewById(R.id.textlLogo);
+
+        hideSystemUI();
 
 
         launchtop = AnimationUtils.loadAnimation(this, R.anim.bounce);
@@ -52,4 +55,17 @@ public class Launch extends AppCompatActivity {
             }
         }, 3500);
     }
+
+    public void hideSystemUI(){
+
+
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
 }
--- app/src/main/java/com/qooplite/alpay/cryptolab/VerschluesselnActivity.java
+++ app/src/main/java/com/qooplite/alpay/cryptolab/VerschluesselnActivity.java
@@ -53,6 +53,8 @@ public class VerschluesselnActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verschluesseln);
 
+        hideSystemUI();
+
         namever = findViewById(R.id.nameec);
         stern1 = findViewById(R.id.stern1ec);
         stern2 = findViewById(R.id.stern2ec);
@@ -177,6 +179,19 @@ public class VerschluesselnActivity extends AppCompatActivity {
         }
 
     }
+
+    public void hideSystemUI(){
+
+
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
 
 
     public void verschluesseln(String verschluesselungsName) {
